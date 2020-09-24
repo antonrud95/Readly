35ef475b9920d1cb8fcd08800b5c837926677fee
arud95 <antonrud95@yandex.ru>
2020-09-24 22:44:02 +0300

    add custom tooltips

--- src/components/ui/activity-graph/activity.js
+++ src/components/ui/activity-graph/activity.js
@@ -16,8 +16,8 @@ const ActivityCard = () => {
             setChartData({
                 labels: [1, 2, 3, 4, 5, 6, 7],
                 datasets: [{
-                    label: 'reviews',
-                    data: [1, 2, 3, 5, 6, 7],
+                    // label: 'reviews',
+                    data: [0, 1, 2, 3, 5, 6, 7],
                     backgroundColor: gradient,
                     borderWidth: 2,
                     borderColor: '#B4BDE9',
@@ -40,7 +40,8 @@ const ActivityCard = () => {
                     min: 0,
                     max: 31,
                     stepSize: 1,
-                    fontSize: 9
+                    fontSize: 9,
+                    beginAtZero: true
                 },
                 gridLines: {
                     display: false,
@@ -73,7 +74,82 @@ const ActivityCard = () => {
         },
         tooltips: {
             enabled: false,
-        } 
+            custom: function(tooltipModel) {
+                // Tooltip Element
+                var tooltipEl = document.getElementById('chartjs-tooltip');
+
+                // Create element on first render
+                if (!tooltipEl) {
+                    tooltipEl = document.createElement('div');
+                    tooltipEl.id = 'chartjs-tooltip';
+                    tooltipEl.innerHTML = '<table></table>';
+                    document.body.appendChild(tooltipEl);
+                }
+
+                // Hide if no tooltip
+                if (tooltipModel.opacity === 0) {
+                    tooltipEl.style.opacity = 0;
+                    return;
+                }
+
+                // Set caret Position
+                tooltipEl.classList.remove('above', 'below', 'no-transform');
+                if (tooltipModel.yAlign) {
+                    tooltipEl.classList.add(tooltipModel.yAlign);
+                } else {
+                    tooltipEl.classList.add('no-transform');
+                }
+
+                function getBody(bodyItem) {
+                    console.log(bodyItem);
+                    return bodyItem.lines;
+                }
+
+                
+
+                // Set Text
+                if (tooltipModel.body) {
+                    var titleLines = tooltipModel.title || [];
+                    var bodyLines = tooltipModel.body.map(getBody);
+                    
+
+                    var innerHtml = '<thead>';
+
+                    titleLines.forEach(function(title) {
+                        innerHtml +=  title + ' ' + 'Sep 22';
+                    });
+                    innerHtml += '</thead><tbody>';
+
+                    bodyLines.forEach(function(body, i) {
+                        // var colors = tooltipModel.labelColors[i];
+                        // var style = 'background:' + colors.backgroundColor;
+                        // style += '; border-color:' + colors.borderColor;
+                        // style += '; border-width: 2px';
+                        // var span = '<span style="' + style + '"></span>';
+                        innerHtml += body + ' ' + 'reviews';
+                        // innerHtml += body + 'reviews';
+                    });
+                    innerHtml += '</tbody>';
+
+                    var tableRoot = tooltipEl.querySelector('table');
+                    tableRoot.innerHTML = innerHtml;
+                }
+
+                // `this` will be the overall tooltip
+                var position = this._chart.canvas.getBoundingClientRect();
+
+                // Display, position, and set styles for font
+                tooltipEl.style.opacity = 1;
+                tooltipEl.style.position = 'absolute';
+                tooltipEl.style.left = position.left + window.pageXOffset + tooltipModel.caretX - 51 + 'px';
+                tooltipEl.style.top = position.top + window.pageYOffset + tooltipModel.caretY - 66 + 'px';
+                tooltipEl.style.fontFamily = tooltipModel._bodyFontFamily;
+                tooltipEl.style.fontSize = tooltipModel.bodyFontSize + 'px';
+                tooltipEl.style.fontStyle = tooltipModel._bodyFontStyle;
+                // tooltipEl.style.padding = tooltipModel.yPadding + 'px ' + tooltipModel.xPadding + 'px';
+                tooltipEl.style.pointerEvents = 'none';
+            }
+        }
     }
         
     return (
