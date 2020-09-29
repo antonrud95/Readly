17695ed5a656a5b60b72f61ff2f66cfdd69cb115
arud95 <antonrud95@yandex.ru>
2020-09-29 19:45:32 +0300

    add toggle active class

--- src/components/pages/explore/explore.js
+++ src/components/pages/explore/explore.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import ActivityCard from '../../ui/activity-graph/activity';
 import Progressbar from '../../ui/progressbar-component/progressbar';
@@ -7,6 +7,24 @@ import Genres from '../../ui/genres-component/genres';
 import './explore.css';
 
 const ExplorePage = () => {
+
+    const [ button, setButton ] = useState({
+        activeObject: null,
+        objects: [{id: 1, name: 'Reviews'}, {id: 2, name: 'Books added'}, {id: 3, name: 'Discussions'}]
+    })
+
+    const toggleActive = index => {
+        setButton({...button, activeObject: button.objects[index]})
+    }
+
+    const toggleActiveStyles = index => {
+        if(button.objects[index] === button.activeObject) {
+            return "buttons-active"
+        } else {
+            return "buttons-inactive"
+        }
+    }
+
     return (
         <div>
             <h2>Explore page does not exist yet...</h2>
@@ -16,11 +34,17 @@ const ExplorePage = () => {
                     <h3>This month</h3>
                 </div>
                 <ActivityCard />
-                <div className="line-wrapper-buttons">
-                    <Link to="/explore/reader-activity" className="line-wrapper-buttons__link">Reviews</Link>
-                    <Link to="/explore/reader-activity" className="line-wrapper-buttons__link">Books added</Link>
-                    <Link to="/explore/reader-activity" className="line-wrapper-buttons__link">Discussions</Link>
+                <div className="buttons">
+                    {button.objects.map((item, index) => (
+                        <Link to="/explore/reader-activity" key={index} className={[toggleActiveStyles(index), 'button-default'].join(' ')} onClick={() => {toggleActive(index)}}>{item.name}</Link>
+                        )
+                    )}
                 </div>
+                {/* <div className="buttons">
+                    <Link to="/explore/reader-activity" className="buttons-active button-default">Reviews</Link>
+                    <Link to="/explore/reader-activity" className="buttons-inactive button-default">Books added</Link>
+                    <Link to="/explore/reader-activity" className="buttons-inactive button-default">Discussions</Link>
+                </div> */}
             </div>
             <div className="progress-wrapper">
                 <Progressbar percentage={72} text={'72%'} pathColor={'#3B5DD6'} trailColor={'#EBEFF4'} textColor={'#1E2C40'}/>
