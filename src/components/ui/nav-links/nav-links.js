d36cf2aceb6678258b45776cc31eb1ff76ebb94b
arud95 <antonrud95@yandex.ru>
2020-09-30 20:38:25 +0300

    add focus navitems styles

--- src/components/ui/nav-links/nav-links.js
+++ src/components/ui/nav-links/nav-links.js
@@ -5,7 +5,7 @@ import './nav-links.css';
 
 import icon from '../../../assets/icons/logo-icon/logo.svg';
 
-import icon1 from '../../../assets/icons/nav-icons/1.svg';
+// import icon1 from '../../../assets/icons/nav-icons/1.svg';
 import icon2 from '../../../assets/icons/nav-icons/2.svg';
 import icon3 from '../../../assets/icons/nav-icons/3.svg';
 import icon4 from '../../../assets/icons/nav-icons/4.svg';
@@ -29,7 +29,9 @@ const NavLinks = () => {
                 </li>
                 <li>
                     <NavLink to="/dashboard" className='nav-item item'>
-                        <img src={icon1}  alt="logo" className='img-logo'/>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                        <path d="M3 3H21C22.1046 3 23 3.89543 23 5V19C23 20.1046 22.1046 21 21 21H3C1.89543 21 1 20.1046 1 19V5C1 3.89543 1.89543 3 3 3ZM3 5V19H21V5H3ZM7 17H9V11H7V17ZM13 17H11V7H13V17ZM15 17H17V10H15V17Z" fill="#8493A9" className="svg-icon"/>
+                        </svg>
                         <p>Dashboard</p>
                     </NavLink>
                 </li>
