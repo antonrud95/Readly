50c6c4e3f221b919bbdb98d796d6164840e26492
arud95 <antonrud95@yandex.ru>
2020-09-30 22:05:20 +0300

    add go back button

--- src/components/sections/all-books/all-books.js
+++ src/components/sections/all-books/all-books.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { useHistory } from "react-router-dom";
 
 import './all-books.css';
 
 const SeeAll = () => {
+    let history = useHistory();
     return (
         <div className="shadow">
             <h2>Go away, there is no books here!</h2>
+            <button onClick={() => history.goBack()}>Back</button>
         </div>
     )
 }
